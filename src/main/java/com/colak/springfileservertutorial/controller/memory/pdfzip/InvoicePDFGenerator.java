793fc4765665f3c4c227a43d644f9c5e51f01116
Orçun Çolak <orcun.colak@hazelcast.com>
2024-04-30 11:42:35 +0300

    move invoice generator to new class

---
+++ src/main/java/com/colak/springfileservertutorial/controller/memory/pdfzip/InvoicePDFGenerator.java
@@ -0,0 +1,78 @@
+package com.colak.springfileservertutorial.controller.memory.pdfzip;
+
+import com.itextpdf.io.image.ImageDataFactory;
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.layout.Document;
+import com.itextpdf.layout.element.Image;
+import com.itextpdf.layout.element.Paragraph;
+import com.itextpdf.layout.element.Table;
+import com.itextpdf.layout.properties.TextAlignment;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.net.URL;
+
+// Example class that show how to convert a Java object to PDF byte array using itext library
+@Slf4j
+class InvoicePDFGenerator {
+
+    public byte[] generatePDF(Invoice invoice) throws IOException {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        try (PdfWriter writer = new PdfWriter(outputStream);
+             PdfDocument pdfDocument = new PdfDocument(writer);
+             Document document = new Document(pdfDocument)) {
+            addTitleText(document, "Dynamic title invoice 2024");
+            // addTitleImage(document, invoice.getTitleImg(), false);
+            addInvoiceTable(document, invoice);
+        }
+        return outputStream.toByteArray();
+    }
+
+    private void addTitleImage(Document document, String titleImage, boolean isExternalImg) {
+        Image image = null;
+        try {
+            if (!isExternalImg) {
+                image = new Image(ImageDataFactory.create(titleImage));
+            } else {
+                image = new Image(ImageDataFactory.create(new URL(titleImage)));
+            }
+            image.setWidth(100);
+            image.setTextAlignment(TextAlignment.LEFT);
+            document.add(image);
+        } catch (Exception exception) {
+            log.error("Error to get img", exception);
+        }
+    }
+
+    // Title above the table
+    private void addTitleText(Document document, String titleText) {
+        Paragraph paragraph = new Paragraph(titleText).setBold().setFontSize(20).setMarginTop(20);
+        paragraph.setTextAlignment(TextAlignment.CENTER);
+        document.add(paragraph);
+    }
+
+    private void addInvoiceTable(Document document, Invoice invoice) {
+        Table table = new Table(2);
+        // Row
+        table.addCell("Invoice Number");
+        table.addCell(invoice.getInvoiceNumber());
+
+        // Row
+        table.addCell("Client Name");
+        table.addCell(invoice.getClientName());
+
+        // Row
+        table.addCell("Date");
+        table.addCell(invoice.getDate().toString());
+
+        // Row
+        table.addCell("Details");
+        com.itextpdf.layout.element.List list = new com.itextpdf.layout.element.List();
+        invoice.getDetails().forEach(list::add);
+        table.addCell(list);
+
+        document.add(table);
+    }
+}
--- src/main/java/com/colak/springfileservertutorial/controller/memory/pdfzip/PDFGenerator.java
+++ src/main/java/com/colak/springfileservertutorial/controller/memory/pdfzip/PDFGenerator.java
@@ -1,18 +1,9 @@
 package com.colak.springfileservertutorial.controller.memory.pdfzip;
 
-import com.itextpdf.io.image.ImageDataFactory;
-import com.itextpdf.kernel.pdf.PdfDocument;
-import com.itextpdf.kernel.pdf.PdfWriter;
-import com.itextpdf.layout.Document;
-import com.itextpdf.layout.element.Image;
-import com.itextpdf.layout.element.Paragraph;
-import com.itextpdf.layout.element.Table;
-import com.itextpdf.layout.properties.TextAlignment;
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.net.URL;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -33,6 +24,7 @@ class PDFGenerator {
     // for best performance
     public byte[] generatePDFsIn(List<Invoice> invoices) throws IOException, InterruptedException {
         ByteArrayOutputStream zipStream = new ByteArrayOutputStream();
+        InvoicePDFGenerator invoicePDFGenerator = new InvoicePDFGenerator();
         try (ZipOutputStream zipOutputStream = new ZipOutputStream(zipStream)) {
             for (int i = 0; i < invoices.size(); i++) {
                 Invoice invoice = invoices.get(i);
@@ -40,7 +32,7 @@ class PDFGenerator {
                 final int index = i;
                 executorService.execute(() -> {
                     try {
-                        byte[] pdfContent = generatePDF(invoice);
+                        byte[] pdfContent = invoicePDFGenerator.generatePDF(invoice);
                         synchronized (zipOutputStream) {
                             zipOutputStream.putNextEntry(new ZipEntry("invoice_" + (index + 1) + ".pdf"));
                             zipOutputStream.write(pdfContent);
@@ -59,10 +51,11 @@ class PDFGenerator {
 
     public byte[] generatePDFs(List<Invoice> invoices) throws IOException {
         ByteArrayOutputStream zipStream = new ByteArrayOutputStream();
+        InvoicePDFGenerator invoicePDFGenerator = new InvoicePDFGenerator();
         try (ZipOutputStream zipOutputStream = new ZipOutputStream(zipStream)) {
             for (int i = 0; i < invoices.size(); i++) {
                 Invoice invoice = invoices.get(i);
-                byte[] pdfContent = generatePDF(invoice);
+                byte[] pdfContent = invoicePDFGenerator.generatePDF(invoice);
                 zipOutputStream.putNextEntry(new ZipEntry("invoice_" + (i + 1) + ".pdf"));
                 zipOutputStream.write(pdfContent);
                 zipOutputStream.closeEntry();
@@ -71,62 +64,6 @@ class PDFGenerator {
         return zipStream.toByteArray();
     }
 
-    private byte[] generatePDF(Invoice invoice) throws IOException {
-        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-        try (PdfWriter writer = new PdfWriter(outputStream);
-             PdfDocument pdfDocument = new PdfDocument(writer);
-             Document document = new Document(pdfDocument)) {
-            addTitleText(document, "Dynamic title invoice 2024");
-            // addTitleImage(document, invoice.getTitleImg(), false);
-            addInvoiceTable(document, invoice);
-        }
-        return outputStream.toByteArray();
-    }
 
-    private void addTitleImage(Document document, String titleImage, boolean isExternalImg) {
-        Image image = null;
-        try {
-            if (!isExternalImg) {
-                image = new Image(ImageDataFactory.create(titleImage));
-            } else {
-                image = new Image(ImageDataFactory.create(new URL(titleImage)));
-            }
-            image.setWidth(100);
-            image.setTextAlignment(TextAlignment.LEFT);
-            document.add(image);
-        } catch (Exception exception) {
-            log.error("Error to get img", exception);
-        }
-    }
-
-    // Title above the table
-    private void addTitleText(Document document, String titleText) {
-        Paragraph paragraph = new Paragraph(titleText).setBold().setFontSize(20).setMarginTop(20);
-        paragraph.setTextAlignment(TextAlignment.CENTER);
-        document.add(paragraph);
-    }
-
-    private void addInvoiceTable(Document document, Invoice invoice) {
-        Table table = new Table(2);
-        // Row
-        table.addCell("Invoice Number");
-        table.addCell(invoice.getInvoiceNumber());
-
-        // Row
-        table.addCell("Client Name");
-        table.addCell(invoice.getClientName());
-
-        // Row
-        table.addCell("Date");
-        table.addCell(invoice.getDate().toString());
-
-        // Row
-        table.addCell("Details");
-        com.itextpdf.layout.element.List list = new com.itextpdf.layout.element.List();
-        invoice.getDetails().forEach(list::add);
-        table.addCell(list);
-
-        document.add(table);
-    }
 
 }
